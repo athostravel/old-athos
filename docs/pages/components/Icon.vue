936c53f3c6fa09728e0909e16b2b359b3e13f591
Javi Puche <javierpuchevictoria@gmail.com>
2019-10-22 15:05:12 +0200

    Remove unused component import

--- docs/pages/components/Icon.vue
+++ docs/pages/components/Icon.vue
@@ -14,14 +14,12 @@
     import Layout from '@/layouts/Default.vue'
     import CodePreview from '@/components/CodePreview/CodePreview.vue'
     import Markdown from '@/components/Markdown/Markdown.vue'
-    import Message from '@/components/Message/Message.vue'
 
     export default {
         components: {
             Layout,
             CodePreview,
-            Markdown,
-            Message
+            Markdown
         }
     }
 </script>
