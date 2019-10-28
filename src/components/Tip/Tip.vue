c62cc7d24d0ae592863d2913b4e7a88dbc15b6c1
Javi Puche <javierpuchevictoria@gmail.com>
2019-10-28 14:02:46 +0100

    Fix identation

--- src/components/Tip/Tip.vue
+++ src/components/Tip/Tip.vue
@@ -1,9 +1,13 @@
 <template>
-    <component :is="tag" class="c-tip" :class="{
-        'c-tip--has-img': img !== undefined,
-        'c-tip--has-icon': icon !== undefined,
-        'c-tip--reverse': reverse !== undefined
-    }">
+    <component
+        :is="tag"
+        class="c-tip"
+        :class="{
+            'c-tip--has-img': img !== undefined,
+            'c-tip--has-icon': icon !== undefined,
+            'c-tip--reverse': reverse !== undefined
+        }"
+    >
         <div class="c-tip__media">
             <AtIcon
                 v-if="icon"
@@ -20,8 +24,8 @@
             />
         </div>
         <div class="c-tip__info">
-            <span class="c-tip__title">{{title}}</span>
-            <span class="c-tip__text">{{text}}</span>
+            <span class="c-tip__title">{{ title }}</span>
+            <span class="c-tip__text">{{ text }}</span>
         </div>
     </component>
 </template>
@@ -59,53 +63,53 @@
 </script>
 
 <style lang="scss">
-    .c-tip {
-        $this: &;
+  .c-tip {
+    $this: &;
 
-        display: inline-flex;
-        align-items: center;
+    display: inline-flex;
+    align-items: center;
 
-        &--reverse {
-            flex-direction: row-reverse;
+    &--reverse {
+      flex-direction: row-reverse;
 
-            #{$this}__media {
-                margin-right: 0;
-                margin-left: 1.2rem;
-            }
-        }
-
-        &__media {
-            align-items: center;
-            display: flex;
-            flex-shrink: 0;
-            justify-content: center;
-            margin-right: 1.2rem;
-            min-height: 40px;
-            min-width: 40px;
-            position: relative;
-        }
-
-        &__icon {
-            font-size: var(--c-input-icon-font-size, 1.8em);
-            color: var(--color-secondary);
-        }
-
-        &__info {
-            display: flex;
-            flex-direction: column;
-        }
-
-        &__title {
-            color: var(--color-primary);
-            font-size: var(--font-size-m);
-            line-height: var(--line-height-xl);
-            font-weight: 700;
-        }
-
-        &__text {
-            color: var(--color-primary);
-            font-size: var(--font-size-xs);
-            line-height: var(--line-height-m);
-        }
+      #{$this}__media {
+        margin-right: 0;
+        margin-left: 1.2rem;
+      }
     }
+
+    &__media {
+      align-items: center;
+      display: flex;
+      flex-shrink: 0;
+      justify-content: center;
+      margin-right: 1.2rem;
+      min-height: 40px;
+      min-width: 40px;
+      position: relative;
+    }
+
+    &__icon {
+      font-size: var(--c-input-icon-font-size, 1.8em);
+      color: var(--color-secondary);
+    }
+
+    &__info {
+      display: flex;
+      flex-direction: column;
+    }
+
+    &__title {
+      color: var(--color-primary);
+      font-size: var(--font-size-m);
+      line-height: var(--line-height-xl);
+      font-weight: 700;
+    }
+
+    &__text {
+      color: var(--color-primary);
+      font-size: var(--font-size-xs);
+      line-height: var(--line-height-m);
+    }
+  }
 </style>
